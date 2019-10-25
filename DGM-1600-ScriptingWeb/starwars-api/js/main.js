3c8907f5022aa5c04fa4d205398337c92c49d46c
Tyson Cottam <tysoncottam@gmail.com>
2019-10-25 00:23:54 -0600

    nav update

--- DGM-1600-ScriptingWeb/starwars-api/js/main.js
+++ DGM-1600-ScriptingWeb/starwars-api/js/main.js
@@ -5,17 +5,20 @@ let mainArea = document.querySelector('main')
 people.forEach((person) => {
     let personDiv = document.createElement('div')
     let name = document.createElement('h2')
+    let eyeColor = document.createElement('p')
     let gender = document.createElement('p')
     let pic = document.createElement('img')
     
     let charNum = getCharNumber(person.url)
     
     name.textContent = person.name
+    eyeColor.textContent = 'Eye Color: ' + person.eye_color.toUpperCase()
     gender.textContent = 'Gender: ' + person.gender.toUpperCase()
     pic.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
     
     personDiv.appendChild(name)
     personDiv.appendChild(pic)
+    personDiv.appendChild(eyeColor)
     personDiv.appendChild(gender)
     
     mainArea.appendChild(personDiv)

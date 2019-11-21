f71f158ab26988551024331933bbf74bded26d83
Tyson Cottam <tysoncottam@gmail.com>
2019-11-21 15:47:58 -0700

    pokemon-part-finished

--- DGM-1600-ScriptingWeb/baseball-project/js/main.js
+++ DGM-1600-ScriptingWeb/baseball-project/js/main.js
@@ -1,3 +1,35 @@
+
+class Pokemon 
+{
+    constructor(id, name, height)
+    {
+        this.id = id
+        this.name = name
+        this.height = height
+    }
+}
+
+const Tyson = new Pokemon(900, 'tyson', 70)
+
+const newButton = document.querySelector('#new-pokemon-button')
+newButton.addEventListener('click', function()
+{
+    let pokeId = prompt("Please enter a Pokemon ID")
+    if(pokeId > 0 && pokeId <=807)
+        {
+            getAPIData(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
+            .then(result =>
+            {
+                populateDOM(result)
+            })
+        }
+    else
+        {
+            alert('Nope. Try again.')
+        }
+       
+})
+
 async function getAPIData(url)
 {
     try
@@ -14,7 +46,7 @@ async function getAPIData(url)
 
 /*getPokemonData('https://pokeapi.co/api/v2/pokemon/')*/
 
-const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/')
+const theData = getAPIData('https://pokeapi.co/api/v2/pokemon/?limit=32')
 .then(data => 
 {
     for (const pokemon of data.results)
@@ -31,26 +63,56 @@ let mainArea = document.querySelector('main')
 
 function populateDOM(single_pokemon) 
 {
+    let scene = document.createElement('div')
+    let card = document.createElement('div')
     let pokeDiv = document.createElement('div')
+    let secondDivForFlip = document.createElement('div')
     let name = document.createElement('h3')
-    let height = document.createElement('p')
+
     let pic = document.createElement('img')
 
-    pokeDiv.setAttribute('class', 'charDivs')
-    pic.setAttribute('class', 'picDivs')
+    fillCardBack(secondDivForFlip, single_pokemon)
+    
+    scene.setAttribute('class', 'scene')
+    card.setAttribute('class', 'card')
+    card.setAttribute('id', 'toggle-back')
+    pokeDiv.setAttribute('class', 'charDivs card-front')
+    secondDivForFlip.setAttribute('class', 'charDivs card-back')
+    pic.setAttribute('class', 'picDivs')    
 
     let pokeNum = getPokeNumber(single_pokemon.id)
 
-    name.textContent = single_pokemon.name
-    height.textContent = single_pokemon.height
-
-    pic.src = `./images/${pokeNum}.png`
+    name.textContent = single_pokemon.name[0].toUpperCase()+single_pokemon.name.slice(1)
+    
+    pic.src = `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${pokeNum}.png`
 
     pokeDiv.appendChild(name)
     pokeDiv.appendChild(pic)
-    pokeDiv.appendChild(height)
 
-    mainArea.appendChild(pokeDiv)
+    card.appendChild(pokeDiv)
+    card.appendChild(secondDivForFlip)
+
+    scene.appendChild(card)
+    mainArea.appendChild(scene)
+    
+    card.addEventListener( 'click', function() 
+    {
+        card.classList.toggle('is-flipped');
+    });
+    
+}
+
+function fillCardBack(secondDivForFlip, data)
+{
+    let type = document.createElement('h3')
+    let pokeHP = document.createElement('h3')
+    let height = document.createElement('h3')
+    type.textContent = `Type: ${data.types[0].type.name[0].toUpperCase()+data.types[0].type.name.slice(1)}`
+    pokeHP.textContent = `HP: ${data.stats[0].base_stat}`
+    height.textContent = `Height: ${data.height}`
+    secondDivForFlip.appendChild(type)
+    secondDivForFlip.appendChild(pokeHP)
+    secondDivForFlip.appendChild(height)
 }
 
 function getPokeNumber(id)
@@ -60,18 +122,5 @@ function getPokeNumber(id)
     {
         return `0${id}`
     } else return id
-    
-    
-    /*
-    let end = charURL.lastIndexOf('/')
-    let charID = charURL.substring(end -2, end)
-    
-    if(charID.indexOf('/') !== -1)
-        {
-            return `00${charID.slice(1,2)}`
-        }
-    else
-        {
-            return `0${charID}`
-        }*/
 }
+

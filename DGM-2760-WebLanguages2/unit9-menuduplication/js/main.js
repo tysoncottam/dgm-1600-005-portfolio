9596df1f421bcbf321d7987f458b115256bbe020
Tyson Cottam <tysoncottam@gmail.com>
2020-04-16 20:46:25 -0600

    cozy deploy

---
+++ DGM-2760-WebLanguages2/unit9-menuduplication/js/main.js
@@ -0,0 +1,15 @@
+function duplicateMenu()
+{
+    // get all the anchor elements from the top menu
+    let topList = document.querySelectorAll('ul#primaryNavigation li a')
+    
+    // create the new list items for the bottom of the page
+    let newList = document.createElement('ul')
+    
+    topList.forEach(menuItem =>{
+        let newLI = document.createElement('li')
+        
+    })
+}
+
+duplicateMenu()

f6d909cb8dc9d3ea15db2ef9b4620f60f634436e
Tyson Cottam <tysoncottam@gmail.com>
2020-04-23 22:14:55 -0600

    gsap animation

--- DGM-2780-Frameworks/project3-cozy/js/index.js
+++ DGM-2780-Frameworks/project3-cozy/js/index.js
@@ -55,3 +55,49 @@ function myFunction4()
     $("#my-header-menu2").removeClass('active');
     $("#my-menu2").removeClass('active');
 }
+
+//--------------------------------------------------------------
+
+//GSAP
+
+window.onload = function() {
+    console.log('it is working');
+    gsap.to(".hero-info",{
+        duration: 1,
+        opacity: 1,
+        y: 30
+        });
+    };
+
+$(window).scroll(function() {
+   var hT = $('.read-section').offset().top,
+       hH = $('.read-section').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+   if (wS > (hT+hH-wH)){
+       gsap.to(".animate-message-left",{
+        duration: 1,
+        opacity: 1,
+        x: 0
+        });
+       gsap.to(".animate-message-right",{
+        duration: 1,
+        opacity: 1,
+        x: 0
+        });
+   }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
